795597ef41574440aa95d53f25d0e0e31d52e5cc
emys <emys@mail.ru>
2022-08-30 12:15:59 +0300

    book_card

--- tasks/easy/incapsulation/book_card.py
+++ tasks/easy/incapsulation/book_card.py
@@ -15,3 +15,65 @@
 from datetime import date
 
 CURRENT_YEAR = date.today().year
+
+
+class BookCard:
+    __author: str
+    __title: str
+    __year: int
+
+    def __init__(self, author, title, year):
+        self.author = author
+        self.title = title
+        self.year = year
+
+    def __eq__(self, other):
+        return self.__year == other
+
+    def __ne__(self, other):
+        return self.__year != other
+
+    def __lt__(self, other):
+        return self.__year < other
+
+    def __le__(self, other):
+        return self.__year <= other
+
+    def __gt__(self, other):
+        return self.__year > other
+
+    def __ge__(self, other):
+        return self.__year >= other
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def year(self):
+        return self.__year
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, str):
+            self.__author = value
+        else:
+            raise ValueError('Expected a string')
+
+    @title.setter
+    def title(self, t):
+        if isinstance(t, str):
+            self.__title = t
+        else:
+            raise ValueError('Expected a string')
+
+    @year.setter
+    def year(self, y):
+        if isinstance(y, int) and 0 < y <= CURRENT_YEAR:
+            self.__year = y
+        else:
+            raise ValueError('Not valid year')
