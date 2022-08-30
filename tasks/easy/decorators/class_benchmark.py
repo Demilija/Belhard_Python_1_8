6db3462f65465b255d6da19f9176d07e6435d53a
emys <emys@mail.ru>
2022-08-30 13:34:34 +0300

    class_benchmark

--- tasks/easy/decorators/class_benchmark.py
+++ tasks/easy/decorators/class_benchmark.py
@@ -20,3 +20,41 @@ import time
 # start_time = time.time()
 # end_time = time.time()
 # difference = e - s
+
+
+def def_benchmark(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        print(f"Выполняем {func.__name__} с args: {args} и kwargs: {kwargs})")
+        print(f"Время начала: {start_time}")
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Выполнено {func.__name__}\nВремя окончания: {end_time}")
+        print(f"Всего затрачено времени на выполнение: {end_time - start_time}")
+        return result
+    return wrapper
+
+
+def class_benchmark(cls):
+    call_attr = {k: v for k, v in cls.__dict__.items() if callable(v) and k[0] != '_'}
+    for name, value in call_attr.items():
+        decorated = def_benchmark(value)
+        setattr(cls, name, decorated)
+    return cls
+
+
+@class_benchmark
+class A:
+    def __init__(self):
+        print('init')
+
+    def hello(self):
+        print('hello')
+
+
+a = A()
+b = A()
+
+
+a.hello()
+b.hello()
