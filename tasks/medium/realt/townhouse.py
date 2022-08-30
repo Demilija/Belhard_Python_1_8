44909350b3abee49fa38a815f0dbe10d8056af1f
emys <emys@mail.ru>
2022-08-30 13:21:51 +0300

    realt

---
+++ tasks/medium/realt/townhouse.py
@@ -0,0 +1,7 @@
+from tasks.medium.realt.house import House
+
+
+class Townhouse(House):
+    def __init__(self, address, area, cost):
+        super().__init__(address, area, cost)
+        self.area = 60
